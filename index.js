518ae9e5ddf2fbd9e57d372a5d51d60667770f56
PatrickSud <101604598+PatrickSud@users.noreply.github.com>
2026-04-16 20:14:42 -0300

    Add new contacts to the contas array

--- index.js
+++ index.js
@@ -58,11 +58,13 @@ client.initialize();
 // ==========================================
 const contas = [
     { nome: 'Jaqueline', telefone: '19971673522', senha: 'Pagy2015', recebeWhatsApp: true, telefoneWhatsApp: '19995487421'},
-    { nome: 'Karen', telefone: '19996722502', senha: 'Vixx140814', email: 'karensgodoy93@gmail.com', recebeWhatsApp: true},
     { nome: 'Gonzalo', telefone: '1931997599', senha: 'Pagy2015', recebeWhatsApp: true},
     { nome: 'Magaly', telefone: '19971691705', senha: 'Andrea1993_!', recebeWhatsApp: true},
     { nome: 'Daniel', telefone: '19998185339', senha: '@bt3RWqUTy.qi', recebeWhatsApp: true, telefoneWhatsApp: '19995487421'},
-    { nome: 'Devania', telefone: '19992509897', senha: 'Vixx140814', email: 'devaniaekaren@gmail.com', recebeWhatsApp: true },
+    { nome: 'Karen', telefone: '19996722502', senha: 'Vixx140814', email: 'karensgodoy93@gmail.com', recebeWhatsApp: true},
+    { nome: 'Devania', telefone: '19992509897', senha: 'Vixx140814', recebeWhatsApp: true },
+    { nome: 'Alvaro', telefone: '11948187041', senha: 'Vix140814', recebeWhatsApp: true },
+    { nome: 'Alvaro 2', telefone: '119481870411', senha: 'Vixx140814', recebeWhatsApp: true, telefoneWhatsApp: '11948187041' },
     { nome: 'Daniel Prieto', telefone: '993940008', senha: 'DSP199s.', recebeWhatsApp: true, telefoneWhatsApp: '19998185339' }
 ];
 
